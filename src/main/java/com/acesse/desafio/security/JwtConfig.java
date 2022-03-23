9bea7e8e7e776cee0d8de362044a5e47c93da475
Braian Ferraz <braianfs2009@gmail.com>
2022-03-23 04:05:52 -0300

    Cors Config

--- src/main/java/com/acesse/desafio/security/JwtConfig.java
+++ src/main/java/com/acesse/desafio/security/JwtConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.acesse.desafio.service.UserDetailsServiceImpl;
 
@@ -44,15 +46,5 @@ public class JwtConfig extends WebSecurityConfigurerAdapter{
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
-	@Bean
-    CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowCredentials(true);
-        configuration.addAllowedOrigin("https://teste-1175a.web.app");
-        configuration.addAllowedHeader("*");
-        configuration.addAllowedMethod("*");
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+	
 }
